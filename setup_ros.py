cb7067d14bd8a9047a6c81fe38bd9eab4b33e4fb
sheosi <sertorbe@gmail.com>
2021-02-10 20:17:15 +0100

    Add compatibility for versions  with Ubuntu 20..04

--- setup_ros.py
+++ setup_ros.py
@@ -3,16 +3,31 @@ import os
 from os.path import expanduser
 import random
 
+ros1_dist_mapping = {
+    "bionic": "melodic",
+    "focal": "noetic"
+}
+
+ros2_dist_mapping = {
+    "bionic": "eloquent",
+    "focal": "foxy"
+}
+
 ## Common initialization
 os.system("sudo apt update")
-os.system("sudo apt -y install curl gnupg2 lsb-release ")
+os.system("sudo apt -y install curl gnupg2 lsb-release")
+
+lsb_rel = subprocess.check_output(["lsb_release", "-sc"]).strip()
+
+ros1_distro = ros1_dist_mapping[lsb_rel]
+ros2_distro = ros2_dist_mapping[lsb_rel]
 
 ## ROS 1
 # Install ROS 1 base
 os.system("sudo sh -c 'echo \"deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main\" > /etc/apt/sources.list.d/ros-latest.list'")
 os.system("sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654")
 os.system("sudo apt update")
-os.system("sudo apt -y install ros-melodic-desktop")
+os.system(f"sudo apt -y install ros-{ros1_distro}-desktop")
 os.system("source ~/.bashrc")
 
 os.system("sudo apt -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential")
@@ -31,20 +46,24 @@ os.system("sudo apt -y install python3-vcstool")
 
 # Install Nao things
 os.chdir(expanduser("~") + "/catkin_ws/src")
-# Base nao
-os.system("sudo apt -y ros-melodic-nao-meshes ros-melodic-naoqi-bridge-msgs ros-melodic-camera-info-manager ros-melodic-octomap-msgs ros-melodic-move-base-msgs liboctomap-dev")
-# NaoGazebo
-os.system("sudo apt -y install ros-melodic-gazebo-ros ros-melodic-controller-manager")
-os.system("git clone https://github.com/ros-naoqi/nao_moveit_config")
-os.system("git clone https://github.com/ros-naoqi/nao_extras")
-os.system("git clone https://github.com/ros-naoqi/naoqi_bridge")
-os.system("git clone https://github.com/ros-naoqi/nao_robot")
-os.system("git clone https://github.com/ros-naoqi/nao_virtual")
-os.system("git clone https://github.com/ahornung/humanoid_msgs/")
-os.chdir(expanduser("~") + "/catkin_ws/")
-os.system("catkin_make")
-# Much easier just to call rosdep, actually
-os.system("rosdep install -i --from-path src --rosdistro melodic -y")
+
+if lsb_rel == "melodic":
+    # Base nao
+    os.system(f"sudo apt -y ros-{ros1_distro}-nao-meshes ros-{ros1_distro}-naoqi-bridge-msgs ros-{ros1_distro}-camera-info-manager ros-{ros1_distro}-octomap-msgs ros-{ros1_distro}-move-base-msgs liboctomap-dev")
+    # NaoGazebo
+    os.system(f"sudo apt -y install ros-{ros1_distro}-gazebo-ros ros-{ros1_distro}-controller-manager")
+    os.system("git clone https://github.com/ros-naoqi/nao_moveit_config")
+    os.system("git clone https://github.com/ros-naoqi/nao_extras")
+    os.system("git clone https://github.com/ros-naoqi/naoqi_bridge")
+    os.system("git clone https://github.com/ros-naoqi/nao_robot")
+    os.system("git clone https://github.com/ros-naoqi/nao_virtual")
+    os.system("git clone https://github.com/ahornung/humanoid_msgs/")
+    os.chdir(expanduser("~") + "/catkin_ws/")
+    os.system("catkin_make")
+    # Much easier just to call rosdep, actually
+    os.system(f"rosdep install -i --from-path src --rosdistro {ros1_distro} -y")
+else:
+    print("[WARNING] This version is not compatible with Nao, won't be installed")
 
 ## ROS 2
 # Make sure we have an UTF-8 compatible system
@@ -58,7 +77,7 @@ os.system("export LANG=es_ES.UTF-8")
 os.system("curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -") # Add key
 os.system("sudo sh -c 'echo \"deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main\" > /etc/apt/sources.list.d/ros2-latest.list'")
 os.system("sudo apt update")
-os.system("sudo apt -y install python3-colcon-common-extensions ros-eloquent-desktop")
+os.system(f"sudo apt -y install python3-colcon-common-extensions ros-{ros2_distro}-desktop")
 
 # Install argcomplete (to have autocompletion)
 os.system("sudo apt -y install python3-pip")
@@ -76,13 +95,13 @@ os.system(r'echo "echo \"ROS_DOMAIN_ID: \$ROS_DOMAIN_ID\"" >> ~/.bashrc')
 
 # Add enable_ros1 function to set-up ros1 env
 os.system("echo \"\nenable_ros1() {\" >> ~/.bashrc")
-os.system("echo \"  source /opt/ros/melodic/setup.bash\" >> ~/.bashrc")
+os.system(f"echo \"  source /opt/ros/{ros1_distro}/setup.bash\" >> ~/.bashrc")
 os.system("echo \"  source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc")
 os.system("echo \"}\" >> ~/.bashrc")
 
 # Add enable_ros1 function to set-up ros1 env
 os.system("echo \"\nenable_ros2() {\" >> ~/.bashrc")
-os.system("echo \"  source /opt/ros/eloquent/setup.bash\" >> ~/.bashrc")
+os.system(f"echo \"  source /opt/ros/{ros2_distro}/setup.bash\" >> ~/.bashrc")
 os.system("echo \"  source /usr/share/colcon_cd/function/colcon_cd.sh\" >> ~/.bashrc")
 os.system("echo \"  source ~/dev_ws/install/local_setup.bash\" >> ~/.bashrc")
 os.system("echo \"  export _colcon_cd_root=~/ros2_install\" >> ~/.bashrc")
