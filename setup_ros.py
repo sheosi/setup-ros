a412c646044420c521cddd946eb243439818ee30
Sergio Tortosa <sertorbe@gmail.com>
2022-02-25 03:14:37 -0800

    Fix env vars handling

--- setup_ros.py
+++ setup_ros.py
@@ -69,18 +69,17 @@ if "ros1" in features:
     os.system("sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654")
     os.system("sudo apt update")
     os.system(f"sudo apt -y install ros-{ros1_distro}-desktop")
-    os.system("source ~/.bashrc")
-
+            
     os.system("sudo apt -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential")
     os.system("sudo apt -y install catkin")
-    os.system("sudo rosdep init")
-    os.system("rosdep update")
+    os.system(f"sudo bash -c \"source /opt/ros/{ros1_distro}/setup.bash; rosdep init\"")
+    os.system(f"bash -c \"source /opt/ros/{ros1_distro}/setup.bash; rosdep update\"")
     #os.system("sudo apt -y autoremove") # Clean all remnants
 
     # Setup catkin_ws
     os.system("mkdir -p ~/catkin_ws/src")
     os.chdir(expanduser("~") + "/catkin_ws")
-    os.system("catkin_make")
+    os.system(f"bash -c \"source /opt/ros/{ros1_distro}/setup.bash; catkin_make\"")
 
     # Install vcstool
     os.system("sudo apt -y install python3-vcstool")
@@ -150,6 +149,7 @@ if "ros1" in features:
 
             # Much easier just to call rosdep, actually
             os.system(f"rosdep install -i --from-path src --rosdistro {ros1_distro} -y")
+
         else:
             print("[WARNING] This version is not compatible with Nao, won't be installed")
 
@@ -160,7 +160,7 @@ if "ros2" in features:
     os.system("sudo apt -y install locales")
     os.system("sudo locale-gen es_ES es_ES.UTF-8")
     os.system("sudo update-locale LC_ALL=es_ES.UTF-8 LANG=es_ES.UTF-8")
-    os.system("export LANG=es_ES.UTF-8")
+    os.environ["LANG"] = "es_ES.UTF-8"
 
     # Install ROS 2 base
     os.system("curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -") # Add key
@@ -190,7 +190,7 @@ if "ros2" in features:
         os.system('echo "}\n" >> ~/.bashrc')
 
     os.chdir(expanduser("~") + "/dev_ws")
-    os.system("colcon build")
+    os.system(f"bash -c \"source /opt/ros/{ros2_distro}/setup.bash; colcon build\"")
 
 # Set shell functions and variables
 if "ros1" or "ros2" in features:
@@ -203,19 +203,19 @@ if "ros2" in features:
 
 if "ros1" in features:
     # Add enable_ros1 function to set-up ros1 env
-    os.system("echo \"\nenable_ros1() {\" >> ~/.bashrc")
+    os.system( "echo \"\nenable_ros1() {\" >> ~/.bashrc")
     os.system(f"echo \"  source /opt/ros/{ros1_distro}/setup.bash\" >> ~/.bashrc")
-    os.system("echo \"  source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc")
-    os.system("echo \"}\" >> ~/.bashrc")
+    os.system( "echo \"  source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc")
+    os.system( "echo \"}\" >> ~/.bashrc")
 
 if "ros2" in features:
     # Add enable_ros2 function to set-up ros2 env
-    os.system("echo \"\nenable_ros2() {\" >> ~/.bashrc")
+    os.system( "echo \"\nenable_ros2() {\" >> ~/.bashrc")
     os.system(f"echo \"  source /opt/ros/{ros2_distro}/setup.bash\" >> ~/.bashrc")
-    os.system("echo \"  source /usr/share/colcon_cd/function/colcon_cd.sh\" >> ~/.bashrc")
-    os.system("echo \"  source ~/dev_ws/install/local_setup.bash\" >> ~/.bashrc")
-    os.system("echo \"  export _colcon_cd_root=~/ros2_install\" >> ~/.bashrc")
-    os.system("echo \"}\" >> ~/.bashrc")
+    os.system( "echo \"  source /usr/share/colcon_cd/function/colcon_cd.sh\" >> ~/.bashrc")
+    os.system( "echo \"  source ~/dev_ws/install/local_setup.bash\" >> ~/.bashrc")
+    os.system( "echo \"  export _colcon_cd_root=~/ros2_install\" >> ~/.bashrc")
+    os.system( "echo \"}\" >> ~/.bashrc")
 
 if "ros1" in features and "ros2" in features:
     # With both ros and enable_bridge
